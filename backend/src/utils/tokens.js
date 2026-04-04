160c520f4b6a80b715d2249577ca83f0e3d9e48c
osama drbas <osamadrbas31@gmail.com>
2026-04-04 09:29:12 +0300

    Edit: edited tokens scure to false

--- backend/src/utils/tokens.js
+++ backend/src/utils/tokens.js
@@ -35,7 +35,7 @@ const setTokenCookies = (res, accessToken, refreshToken) => {
   // Set access token cookie (short-lived)
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: isSecure,
+    secure: false,
     sameSite: "none", // Important for cross-domain cookies
     maxAge: 15 * 60 * 1000, // 15 minutes
     path: "/",
@@ -45,7 +45,7 @@ const setTokenCookies = (res, accessToken, refreshToken) => {
   // Set refresh token cookie (long-lived)
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: isSecure,
+    secure: false,
     sameSite: "none", // Important for cross-domain cookies
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: "/api/auth/refresh",

9ce2b6cb1813c0d0b8730c4dfe74659426f0d96e
osama drbas <osamadrbas31@gmail.com>
2026-04-06 06:41:41 +0300

    Edit: edit tokens to be stored in http only cookies

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { LoginForm } from "@/components/auth/LoginForm";
 import { AuthLayout } from "@/components/auth/AuthLayout";
 
 export default function LoginPage() {
-  const searchParams = useSearchParams();
-  const returnUrl = searchParams.get("returnUrl");
-
   return (
     <AuthLayout title="Welcome Back" subtitle="Sign in to your account">
       <LoginForm returnUrl={returnUrl} />

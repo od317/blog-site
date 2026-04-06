db2a12c312ad25d98bc4035d5cbe3e65419cf428
osama drbas <osamadrbas31@gmail.com>
2026-04-06 06:43:49 +0300

    Edit: edit tokens to be stored in http only cookies

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ import { AuthLayout } from "@/components/auth/AuthLayout";
 export default function LoginPage() {
   return (
     <AuthLayout title="Welcome Back" subtitle="Sign in to your account">
-      <LoginForm returnUrl={returnUrl} />
+      <LoginForm returnUrl={""} />
     </AuthLayout>
   );
 }
